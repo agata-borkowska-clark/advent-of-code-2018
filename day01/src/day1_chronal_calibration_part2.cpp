8bbbca372d92f0bfcd668c65a0d2973f4eb77839
Agata Borkowska <borkowska.agata37@gmail.com>
2018-12-01 22:29:49 +0100

    following the first batch of suggestions...

--- day01/src/day1_chronal_calibration_part2.cpp
+++ day01/src/day1_chronal_calibration_part2.cpp
@@ -3,26 +3,18 @@
 #include <string>
 #include <map>
 
-using namespace std;
-
 int main() {
-  ifstream input("input");
+  std::ifstream input("input");
 
   std::map<int, bool> frequencies_seen;
   int current_frequency = 0;
   std::string change;
   while (true) {
     input.clear();
-    input.seekg(0, ios::beg);
+    input.seekg(0, std::ios::beg);
     while (std::getline(input, change)) {
-      std::string abs_value_as_string = change.substr(1);
-      int abs_value_as_int = std::stoi(abs_value_as_string);
-      
-      if (change.at(0) == '+') {
-        current_frequency += abs_value_as_int;
-      } else {
-        current_frequency -= abs_value_as_int;
-      }
+      int change_as_int = std::stoi(change);
+      current_frequency += change_as_int;
 
       if (frequencies_seen[current_frequency] == 1) {
           std::cout << current_frequency << '\n';
