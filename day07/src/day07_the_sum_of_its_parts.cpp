2f36acbc1d670278c5601ee53806784188358264
Agata Borkowska <borkowska.agata37@gmail.com>
2018-12-08 17:32:27 +0100

    day 7 p1 working

--- day07/src/day07_the_sum_of_its_parts.cpp
+++ day07/src/day07_the_sum_of_its_parts.cpp
@@ -9,33 +9,46 @@ int main() {
   std::string entry;
 
   struct node {
-    char name;
     std::set<char> predecessors;
     std::set<char> successors;
-    bool operator < (const node &second) const { 
-      return name < second.name; 
-    }
-    bool operator == (const node &second) const {
-      return name == second.name;
-    }
   };
-  std::set<node> ready;
-  std::set<node> nodes;
+  std::set<char> ready;
+  std::map<char, node> nodes;
   while (std::getline(in, entry)) {
     char name = entry.at(5);
     char successor = entry.at(36);
-    node n;
-    n.name = name;
-    if (nodes.find(n) != nodes.end()) {
-      (*nodes.find(n)).successors.insert(successor);
+    if (nodes.find(name) != nodes.end()) {
+      nodes.find(name)->second.successors.insert(successor);
     } else {
+      node n;
       n.successors.insert(successor);
-      nodes.insert(n);
+      nodes.insert(std::pair<char, node>(name, n));
     }
-    node s;
-    s.name = successor;
-    if (nodes.find(s) != nodes.end()) {
-      (*nodes.find(s)).predecessors.insert(name);
+    if (nodes.find(successor) != nodes.end()) {
+      nodes.find(successor)->second.predecessors.insert(name);
+    } else {
+      node s;
+      s.predecessors.insert(name);
+      nodes.insert(std::pair<char, node>(successor, s));
     }
   }
+  // add all nodes which don't have a predecessor
+  for (auto n : nodes) {
+    if (n.second.predecessors.empty()) {
+      ready.insert(n.first);
+    }
+  }
+  while (!ready.empty()) {
+    char first_in_queue = *ready.begin();
+    std::cout << first_in_queue;
+    ready.erase(first_in_queue);
+    std::pair<char, node> first_node = *nodes.find(first_in_queue);
+    for (auto s : first_node.second.successors) {
+      (*nodes.find(s)).second.predecessors.erase(first_in_queue);
+      if((*nodes.find(s)).second.predecessors.empty()) {
+        ready.insert((*nodes.find(s)).first);
+      }
+    }
+  }
+  std::cout << '\n';
 }
